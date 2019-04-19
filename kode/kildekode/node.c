61a4ed8406bb4ecf90edc8e3dfe25b5006f4a7af
Peter Rasmussen Lubiana <peterrl@nordur.ifi.uio.no>
2019-04-19 11:52:41 +0200

    Kom litt videre i prosjektet.

--- kode/kildekode/node.c
+++ kode/kildekode/node.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h> // For å kunne stenge en socket med close().
+#include <sys/socket.h> // For det standardisérte socket-API'et.
+
 
 char buffer [2048];
 int OwnAddress = -1;
 int BasePort = -1;
 
+void initializeNode(int, char **);
+
 struct NodeInfo{
 	int OwnAddress;
 	int BasePort;
+	int from;
+	int to;
+	int weight;
 };
 
-struct NodeInfo n1;
+int neighbourCount = 0; // Amount of neighbouring nodes.
+struct NodeInfo * neighbourNodes; // Array that holds references to all neighboring nodes and their weights.
 
 
 
-int main(int argc, char* argv[]){
+
+int main(int argc, char * argv[]){
 
 	printf("Hello I'm node.c ! Amount of arguments: %d \n",argc);
-	int i;
-	printf("n1.OwnAddress: %d",n1.OwnAddress);
-	printf("n1.BasePort: %d",n1.BasePort);
-
-	for(i = 0; i < argc ; i++){
-		printf("argument %d: %s \n",i, argv[i]);
+	
+	initializeNode(argc, argv);
 
 
-		if( i == 1 ){
-			BasePort = atoi(argv[i]);
-		}
-
-		if( i == 2 ){
-			OwnAddress = atoi(argv[i]);
-		}
-
-
-		// Store Neighbooring Nodes + Weights.
-		if( i >= 3){
-
-		}
-
-	}
-
+	openTCPConnectionToRoutingServer();
 
 	printNodeInfo();
 
@@ -49,11 +40,78 @@ int main(int argc, char* argv[]){
 }
 
 
+
+
+
+
+
+
+
+void initializeNode(int argCount, char * argList[]){
+	
+    // If the arguments to this program are formatted correctly
+    // argc - 3 should be the correct size of the Edge -> Weight Array.
+    neighbourCount = argCount - 3;
+    neighbourNodes = malloc(sizeof(struct NodeInfo) * neighbourCount);
+
+    int i;
+	for(i = 0; i < argCount ; i++){
+		printf("argument %d: %s \n",i, argList[i]);
+
+
+		if( i == 1 ){
+			BasePort = atoi(argList[i]);
+		}
+
+		if( i == 2 ){
+			OwnAddress = atoi(argList[i]);
+		}
+
+
+		// Lagre Neighbooring Nodes + Weights.
+		if( i >= 3){
+
+            int currentNeighbourIndex = i - neighbourCount;
+			// Så må vi allokere minne til structen som skal lagres.
+			neighbourNodes[currentNeighbourIndex].from = OwnAddress;
+
+			// Leser inn informasjon i structen fra argumentet.
+			sscanf(argList[i], "%d:%d", &neighbourNodes[currentNeighbourIndex].to, &neighbourNodes[currentNeighbourIndex].weight);
+
+		}
+
+	}
+}
+
+
+
+int openTCPConnectionToRoutingServer(){
+
+
+	return 0;
+}
+
+
+void freeAllAllocatedMemory (){
+	free(neighbourNodes);
+}
+
+
+
 int printNodeInfo(){
 	printf("Node Information:::::\n----------------\n");
 	printf("OwnAddress: %d \n", OwnAddress);
 	printf("BasePort  : %d \n", BasePort);
 	printf("\n");
+	int i = 0; 
+	for(; i < neighbourCount ; i++){
+	    printf("frm: %d  to:   %d   weight: %d \n", neighbourNodes[i].from, neighbourNodes[i].to, neighbourNodes[i].weight);
+	}
+}
+
+
+void parseCommandFromUDPSocket(){
+	
 }
 
 
--- kode/kildekode/routing_server.c
+++ kode/kildekode/routing_server.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> // For å ha access til printf()
+
+#include <unistd.h> // For å kunne stenge en socket med close().
+#include <sys/socket.h> // For det standardisérte socket-API'et.
 
 
-int PORT;
-int ;
+
+
+
+int PORT = 0;
+int N = 0;  // N = antall noder i systemet.
 
 int main(int argc, char* argv[]){
 
@@ -10,27 +16,32 @@ int main(int argc, char* argv[]){
 
     int i;
 
-    printf("All Arguments.");
+    printf("All Arguments.\n");
 	for(i = 0; i < argc ; i ++){
 
-		printf("Args : %d \n", argv[i]);
+		printf("argv[%d] : %d \n", i , argv[i]);
 
-		if(argc == 1)
+		if(i == 1)
 			PORT = atoi(argv[i]);
+
+		if(i == 2){
+			N = atoi(argv[i]);
+		}
 	}
 
 
 	// Les data.txt.
 
-	  // Lagre nodene i en datastruktur.
-
-	// Utfør Dijsktras-algoritme for alle nodene og lagre det.
+    // Lagre nodene i en datastruktur.
 
 
 
 
+    // Åpne en Socket som lytter på en bestemt Port
+    // Og vent til Alle de N node-programmene har koblet seg til.
 
-	// Åpne en Socket som lytter på en bestemt Port.
+    // Så må jeg bygge opp en graf og kalkulere dijkstras-algoritme på den.
+	
 
 
 
